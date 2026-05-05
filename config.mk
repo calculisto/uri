3daa721524d35ba8997294f10a27b0735d4a9db6
Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>
2026-05-05 11:19:52 +0200

    Add taocpp/pegtl as a git submodule
    Signed-off-by: Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>

--- config.mk
+++
@@ -1,7 +0,0 @@
-DEPENDENCIES_HEADERS=../../external/taocpp/pegtl/include
-
-PROJECT=uri
-LINK.o=${LINK.cc}
-CXXFLAGS+=-std=c++2a -Wall -Wextra $(foreach dir, ${DEPENDENCIES_HEADERS}, -I../${dir})
-LDLIBS+= -lfmt
-
